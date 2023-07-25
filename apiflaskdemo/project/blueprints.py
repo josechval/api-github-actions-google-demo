3590ce13b7d6b222b8707f47b80c4d920747ac59
Jose Luis Chiquete <josech@gmail.com>
2023-07-25 15:26:41 -0600

    230725a

--- apiflaskdemo/project/blueprints.py
+++ apiflaskdemo/project/blueprints.py
@@ -6,17 +6,17 @@ from marshmallow.exceptions import ValidationError
 
 abc_alumnos = APIBlueprint('abc_alumno', __name__)
 
-@abc_alumnos.get("/")
+@abc_alumnos.get("/alumnos/")
 @abc_alumnos.output(AlumnoSchema(many=True))
 def vuelca_base():
     return Alumno.query.all()
 
-@abc_alumnos.get("/<int:cuenta>")
+@abc_alumnos.get("/alumno/<int:cuenta>")
 @abc_alumnos.output(AlumnoSchema)
 def despliega_alumno(cuenta):
     return Alumno.query.get_or_404(cuenta)
 
-@abc_alumnos.delete("/<int:cuenta>")
+@abc_alumnos.delete("/alumno/<int:cuenta>")
 @abc_alumnos.output(AlumnoSchema)
 @login_required
 def elimina_alumno(cuenta):
@@ -25,23 +25,20 @@ def elimina_alumno(cuenta):
     db.session.commit()
     return alumno
     
-@abc_alumnos.post("/<int:cuenta>")
-@abc_alumnos.output(AlumnoSchema)
+@abc_alumnos.post("/alumno/<int:cuenta>")
+@abc_alumnos.output(AlumnoSchema, status_code=201)
 @abc_alumnos.input(AlumnoInSchema)
 def crea_alumno(cuenta, data):
     if Alumno.query.filter_by(cuenta=cuenta).first():
         abort(409)
     else: 
         data["cuenta"] = cuenta
-        try:
-            alumno = Alumno(**AlumnoSchema().load(data))
-        except ValidationError:
-            return abort(400)
+        alumno = Alumno(**AlumnoSchema().load(data))
         db.session.add(alumno)
         db.session.commit()
-        return alumno
+        return alumno, 201
 
-@abc_alumnos.put("/<int:cuenta>")
+@abc_alumnos.put("/alumno/<int:cuenta>")
 @abc_alumnos.output(AlumnoSchema)
 @abc_alumnos.input(AlumnoInSchema)
 def sustituye_alumno(cuenta, data):
@@ -51,4 +48,4 @@ def sustituye_alumno(cuenta, data):
     nuevo_alumno = Alumno(**data)
     db.session.add(nuevo_alumno)
     db.session.commit()
-    return nuevo_alumno
+    return  nuevo_alumno
